9b75f829842b998de4c02c75effbe71407fed4db
Jakob Kunzler <jake.kunzler@gmail.com>
2026-02-02 21:08:17 -0500

    Added default RNG

--- src/random/uniform_generator.rs
+++ src/random/uniform_generator.rs
@@ -1,4 +1,8 @@
+use crate::random::xoshiro256plusplus::Xoshiro256PlusPlus;
+
 pub trait UniformGenerator{
     fn next_u64(&mut self) -> u64;
     fn next_f64(&mut self) -> f64;
 }
+
+pub type DefaultUniformRNG = Xoshiro256PlusPlus;
